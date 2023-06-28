007c0510fab3fb2f4ae1d824a87db243029c911b
oopist <oopist@gmail.com>
2023-06-28 12:11:57 +0900

    remove import PaginationConf

--- prunus-nexacro-adaptor-example/src/main/java/nexacro/adaptor/AdaptorConfiguration.java
+++ prunus-nexacro-adaptor-example/src/main/java/nexacro/adaptor/AdaptorConfiguration.java
@@ -1,10 +1,7 @@
 package nexacro.adaptor;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import prunus.persistence.data.pagination.PaginationConfig;
 
-@Import(PaginationConfig.class)
 @Configuration
 public class AdaptorConfiguration {
 }
